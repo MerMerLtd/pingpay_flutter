5d080cda938038559b3b5d375ef25ef6495188b6
TzuHanLiang <crocodilestear@gmail.com>
2019-10-22 13:59:53 +0800

    add images/

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  Center(child: Text("s",),)
+      // Stack(
+      //   fit: StackFit.expand,
+      //   children: <Widget>[
+      //     Container(
+      //       decoration: BoxDecoration(
+      //         color: Color(0xff112233),
+      //       ),
+      //       // child: SvgPicture.asset(
+      //       //   'assets/images.bg_pattern.svg',
+      //       // ),
+      //     ),
+      //     Center(child: Text("s",),)
+      //   ],
+      // ),
+    );
+  }
+}
